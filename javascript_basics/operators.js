f125da77b3294a46d2d404938bc1659602c54699
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-02 09:52:38 -0500

    basic demo of operators

---
+++ javascript_basics/operators.js
@@ -0,0 +1,21 @@
+var x = 10;
+var y = 5;
+var z = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x % z);
+
+x++
+console.log(x)
+x--
+console.log(x)
+
+var shortMessage = 'Hey!';
+var addMessage = 'It is nice to meet you!';
+
+console.log(shortMessage)
+console.log(shortMessage + addMessage)
