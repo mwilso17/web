6cfc9545d53335239e2ebac4793c4a3084f489ce
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-02 09:25:33 -0500

    added variable examples

--- javascript_basics/variables.js
+++ javascript_basics/variables.js
@@ -1,3 +1,14 @@
 var example = 'this is an example of a variable';
 
 console.log(example);
+
+var greeting1 = 'Bob says, "hi."';
+
+console.log(greeting1);
+
+var greeting2 = 'It\'s good to see you.';
+
+console.log(greeting2);
+
+console.log(greeting1.length);
+console.log(greeting1.toUpperCase());
