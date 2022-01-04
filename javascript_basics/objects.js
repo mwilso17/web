ec8c0f256c1d193aa964cb5177c560c03bef097b
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-04 06:58:42 -0500

    basic object in 3 notations

---
+++ javascript_basics/objects.js
@@ -0,0 +1,15 @@
+var human = {
+  hungry: true,
+  age: 25,
+  name: 'Bob',
+};
+
+var pizza = {};
+
+pizza.crust = 'wheat'
+pizza.sauce = 'marinara'
+pizza.cheese = 'extra'
+
+var cup = {};
+cup['insulated'] = true;
+cup['oz'] = 32;
