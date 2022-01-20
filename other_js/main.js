de2bfc3ed0cf952223ef6f29e35cfb3f24c0bc96
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-20 07:04:27 -0500

    missing number algorithm

--- other_js/main.js
+++ other_js/main.js
@@ -3,6 +3,17 @@
  */
 //Single line comment.
 
-var greeting = 'Welcome to JavaScript. I will be your guide!';
+var numbers = [1, 3, 5, 7, 9, 2, 4, 8];
 
-alert(greeting);
+function missingNumber(numbers) {
+  var missing = -1;
+
+  for(i = 0; i <= numbers.length; i++) {
+    if(numbers.indexOf(i) === -1) {
+      missing = i;
+    }
+  }
+  return missing;
+}
+
+console.log(missingNumber(numbers));
