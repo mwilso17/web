4910c2d745396b32293acc0cf41ac2405b4b7146
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-24 06:31:56 -0500

    race registration program

---
+++ other_js/raceRegistration.js
@@ -0,0 +1,17 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+var earlyRegistration = false;
+var runnerAge = 19;
+if (runnerAge >= 19 && earlyRegistration = True) {
+  console.log('Your race time is 9:30am.');
+  console.log(`Your race number is ${raceNumber}.`);
+} else if (runnerAge >= 19 && earlyRegistration = False) {
+  console.log('Your race time is 11:00am.');
+  console.log(`Your race number is ${raceNumber}.`);
+} else if (runnerAge <= 17) {
+   console.log('Your race time is 12:30pm.');
+   console.log(`Your race number is ${raceNumber}.`);
+} else {
+  console.log('Please see the registration desk.')
+}
+
+//Program incomplete. Need to fix bugs
